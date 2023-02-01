0b7060b593663d2169a08be2d722df974474b24d
Mamushe <tewodroslemma64@gmail.com>
2023-02-01 06:40:12 -0500

    initialize proj

---
+++ src/main/java/com/pgdock/pgdock/PgDockApplication.java
@@ -0,0 +1,13 @@
+package com.pgdock.pgdock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgDockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgDockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pgdock/pgdock/PgDockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgdock.pgdock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgDockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
