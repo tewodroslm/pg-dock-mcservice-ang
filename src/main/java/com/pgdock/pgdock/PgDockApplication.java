62528759a161f4893cc66bcefb6eb429e286dd1b
Mamushe <tewodroslemma64@gmail.com>
2023-02-03 14:44:14 -0500

    set which yml to use

--- src/main/java/com/pgdock/pgdock/PgDockApplication.java
+++ src/main/java/com/pgdock/pgdock/PgDockApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PgDockApplication {
 
+
 	public static void main(String[] args) {
+		System.setProperty("spring.config.name", "application-local");
 		SpringApplication.run(PgDockApplication.class, args);
 	}
 
